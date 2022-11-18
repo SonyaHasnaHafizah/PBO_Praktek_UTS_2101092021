a716f90d2b51bbf99254981c5bd87b94b1ee4895
SonyaHasnaHafizah <114122480+SonyaHasnaHafizah@users.noreply.github.com>
2022-11-18 11:57:57 +0700

    Update PeminjamanSonyaExample.java

--- src/UTS181122_B/PeminjamanSonyaExample.java
+++ src/UTS181122_B/PeminjamanSonyaExample.java
@@ -16,20 +16,23 @@ public class PeminjamanSonyaExample {
         nasabah1.setKodepinjam("001");
         nasabah1.setTanggal("171122");
         nasabah1.setKodenasabah("001");
-        nasabah1.setNama("Sonya");
+        nasabah1.setNama("Sonya Hasna Hafizah");
         nasabah1.setJumlah(100000);
         nasabah1.setLama(1);
         
-        System.out.println("Kode Peminjaman: "+nasabah1.getKodepinjam());
-        System.out.println("Tanggal: "+nasabah1.getTanggal());
-        System.out.println("Kode Nasabah: "+nasabah1.getKodenasabah());
-        System.out.println("Nama Nasabah: "+nasabah1.getNama());
-        System.out.println("Jumlah Pinjaman: "+nasabah1.getJumlah());
+        System.out.println("Program Peminjaman");
+        System.out.println("-------------------------------------------------");
+        System.out.println("Kode Peminjaman            : "+nasabah1.getKodepinjam());
+        System.out.println("Tanggal (cth: 181122)      : "+nasabah1.getTanggal());
+        System.out.println("Kode Nasabah               : "+nasabah1.getKodenasabah());
+        System.out.println("Nama Nasabah               : "+nasabah1.getNama());
+        System.out.println("Jumlah Pinjaman            : Rp "+nasabah1.getJumlah());
         System.out.println("Lama Pinjaman (dalam bulan): "+nasabah1.getLama());
-        System.out.println("Bunga : "+nasabah1.getBunga());
-        System.out.println("Angsuran Pokok : "+nasabah1.getAngsuranpokok());
-        System.out.println("Angsuran Perbulan : "+nasabah1.getAngsuranbulan());
-        System.out.println("Total Hutang : "+nasabah1.getHutang());
+        System.out.println("Bunga                      : Rp "+nasabah1.getBunga());
+        System.out.println("Angsuran Pokok             : Rp "+nasabah1.getAngsuranpokok());
+        System.out.println("Angsuran Perbulan          : Rp "+nasabah1.getAngsuranbulan());
+        System.out.println("Total Hutang               : Rp "+nasabah1.getHutang());
+        System.out.println("-----------------Terimakasih :)------------------");
         
     }
 }
